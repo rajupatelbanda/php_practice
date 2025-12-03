4522fe7d4aa02dffec6c3745cbc6a31dd7d6e8c3
rajubanda81@gmail.com <rajubanda81@gmail.com>
2025-12-03 10:40:58 +0530

    this is my first

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+// $apple = "hello world welcome to new";
+
+// echo str_word_count($apple);
+
+
+class Fruit
+{
+          public $name;
+          public $color;
+
+          function __construct($name, $color)
+          {
+                    $this->name = $name;
+                    $this->color = $color;
+          }
+
+          function myFruit()
+          {
+                    return "The fruit is " . $this->name . " and the color is " . $this->color . ".";
+          }
+}
